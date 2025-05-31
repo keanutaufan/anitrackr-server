7bd6d41dddcf9e159a06aba5699cdbcd1944e2c6
Keanu Taufan <keanu.taufan21@gmail.com>
2025-05-31 22:18:50 +0700

    feat: use message as common error/info message in http response

--- pkg/http_response/http_response.go
+++ pkg/http_response/http_response.go
@@ -1,9 +1,8 @@
 package http_response
 
 type Response struct {
-	Success bool   `json:"success"`
-	Info    string `json:"info,omitempty"`
-	Error   error  `json:"error,omitempty"`
-	Data    any    `json:"data"`
-	Meta    any    `json:"meta,omitempty"`
+	Success bool `json:"success"`
+	Message any  `json:"message,omitempty"`
+	Data    any  `json:"data"`
+	Meta    any  `json:"meta,omitempty"`
 }
